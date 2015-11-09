0e943c5237e5dda1b02d71bda1df21291dc43320
Akashosaur <akasharora15@gmail.com>
2015-11-09 01:05:03 -0800

    Ball Movement and Walls 2D Objects

---
+++ 2Player Pong/Assets/Ball1Move.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball1Move : MonoBehaviour {
+
+	public float ballVelocity = 3000;
+	Rigidbody2D rb;
+	bool isPlay;
+	int randInt;
+
+	void Awake () {
+		rb = gameObject.GetComponent<Rigidbody2D> ();
+		randInt = Random.Range (1, 3);
+	}
+	
+
+	void Update () {
+		if (Input.GetKey(KeyCode.Space) == true && isPlay == false) {
+			transform.parent = null;
+			isPlay = true;
+			rb.isKinematic = false;
+			if (randInt == 1) {
+				rb.AddForce(new Vector3(ballVelocity,ballVelocity,0));
+			}
+			if (randInt == 2) {
+				rb.AddForce(new Vector3(-ballVelocity,-ballVelocity,0));
+			}
+
+		}
+	}
+}
