c10435e5217fd39f9a485b0936cdc7f24a20178b
15bstrobel <15bstrobel@gmail.com>
2015-11-09 00:36:46 -0800

    Generic PlayerMove script
    Removed player1 and player2 move.
    
    Updated player objects
    
    added manager script + object

---
+++ 2Player Pong/Assets/PlayerMove.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+	
+	public float speed;
+	public KeyCode up;
+	public KeyCode down;
+
+	float min = -5f;
+	float max = 5f;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void setMinMax(float min, float max) {
+		this.min = min;
+		this.max = max;
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		float movement = 0;
+		if (Input.GetKey(up)) {
+			movement += speed * Time.deltaTime;
+		}
+		if (Input.GetKey(down)) {
+			movement -= speed * Time.deltaTime;
+		}
+		movement += gameObject.transform.position.y;
+		movement = Mathf.Clamp(movement, min, max);
+		gameObject.transform.position = new Vector3(gameObject.transform.position.x, movement, 0);
+	}
+}
